3bc520a5a43aca555d03d088a105927153602e9e
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-08-30 14:52:04 +0400

    Init

---
+++ Fiorello/Fiorello/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Fiorello.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiorello.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
