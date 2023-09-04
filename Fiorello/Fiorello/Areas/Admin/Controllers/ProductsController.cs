e57e31ea55f9bc121789c2a7ad04f86952451392
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-09-05 02:37:17 +0400

    add Views Update and Create

--- Fiorello/Fiorello/Areas/Admin/Controllers/ProductsController.cs
+++ Fiorello/Fiorello/Areas/Admin/Controllers/ProductsController.cs
@@ -19,22 +19,24 @@ namespace Fiorello.Areas.Admin.Controllers
         public ProductsController(AppDbContext db, IWebHostEnvironment env)
         {
             _db = db;
-            _env= env;  
+            _env = env;
         }
         public async Task<IActionResult> Index()
         {
             List<Product> products = await _db.Products.Include(x => x.Category).ToListAsync();
             return View(products);
         }
-        public async Task<IActionResult> Create() 
+        public async Task<IActionResult> Create()
         {
             ViewBag.Categories = await _db.Categories.ToListAsync();
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Product product)
+        public async Task<IActionResult> Create(Product product, int catId)
         {
+            ViewBag.Categories = await _db.Categories.ToListAsync();
+
             #region Is Exist
             bool isExist = await _db.Products.AnyAsync(x => x.Name == product.Name);
             if (isExist)
@@ -64,7 +66,77 @@ namespace Fiorello.Areas.Admin.Controllers
             product.Image = await product.Photo.SaveFileAsync(folder);
             #endregion
 
+            product.CategoryId = catId;
+
             await _db.Products.AddAsync(product);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product dbProduct = await _db.Products.FirstOrDefaultAsync();
+
+            if (dbProduct == null)
+            {
+                return BadRequest();
+            }
+
+            ViewBag.Categories = await _db.Categories.ToListAsync();
+            return View(dbProduct);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Product product, int catId)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product dbProduct = await _db.Products.FirstOrDefaultAsync();
+
+            if (dbProduct == null)
+            {
+                return BadRequest();
+            }
+
+            ViewBag.Categories = await _db.Categories.ToListAsync();
+
+            #region Is Exist
+            bool isExist = await _db.Products.AnyAsync(x => x.Name == product.Name && x.Id != id);
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "This product is already exist");
+                return View();
+            }
+            #endregion
+
+            #region Save Image
+            if (product.Photo != null)
+            {
+                if (!product.Photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "please select image type");
+                    return View();
+                }
+                if (product.Photo.IsOrder1Mb())
+                {
+                    ModelState.AddModelError("Photo", "Max 1Mb");
+                    return View();
+                }
+                string folder = Path.Combine(_env.WebRootPath, "img");
+                dbProduct.Image = await product.Photo.SaveFileAsync(folder);
+            }
+            #endregion
+
+            dbProduct.Name= product.Name;
+            dbProduct.Price = product.Price;
+            dbProduct.CategoryId = catId;
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
