80398ed61f7d7d75465d90daef8748f5515308cf
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-09-09 14:13:20 +0400

    Load More with Ajax

--- Fiorello/Fiorello/Controllers/ProductsController.cs
+++ Fiorello/Fiorello/Controllers/ProductsController.cs
@@ -2,7 +2,9 @@
 using Fiorello.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fiorello.Controllers
@@ -16,17 +18,33 @@ namespace Fiorello.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            List<Product> products = await _db.Products.ToListAsync();
+            ViewBag.ProductsCount = await _db.Products.Where(x => !x.IsDeactive).CountAsync();
+            List<Product> products = await _db.Products.Where(x => !x.IsDeactive).OrderByDescending(x => x.Id).Take(8).ToListAsync();
             return View(products);
         }
         public async Task<IActionResult> Detail(int? id)
         {
             if (id == null)
                 return NotFound();
-            Product product = await _db.Products.Include(x=>x.ProductDetail).FirstOrDefaultAsync(x => x.Id == id);
+            Product product = await _db.Products.Include(x => x.ProductDetail).FirstOrDefaultAsync(x => x.Id == id);
             if (product == null)
                 return BadRequest();
             return View(product);
         }
+
+        public async Task<IActionResult> LoadMore(int skipCount)
+        {
+            int count = await _db.Products.Where(x => !x.IsDeactive).CountAsync();
+            if (count <= skipCount)
+            {
+                return Content("Redd ol");
+            }
+
+            List<Product> products = await _db.Products.OrderByDescending(x => x.Id).Skip(skipCount).Take(8).ToListAsync();
+
+            return PartialView("_LoadMoreProductsPartial", products);
+        }
+
+
     }
 }
---
+++ Fiorello/Fiorello/wwwroot/js/loadMore.js
@@ -0,0 +1,22 @@
+﻿let skip = 8;
+
+let productsCounts = $("#loadMore").next().val();
+
+
+$(document).on("click", "#loadMore", function () {
+    $.ajax({
+        url: "/Products/LoadMore/",
+        type: "get",
+        data: {
+            "skipCount": skip
+        },
+        success: function (res) {
+            $("#myProducts").append(res)
+            skip += 8;
+
+            if (productsCounts <= skip) {
+                $("#loadMore").remove()
+            }
+        }
+    });
+});
